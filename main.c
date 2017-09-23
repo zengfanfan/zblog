b4bff87e4306502a8e521135fe7383fc0b2b628e
Zeng Fanfan <hurryzeng@foxmail.com>
2017-09-24 00:25:03 +0800

    make path relative

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
 #include <lib/holyhttp.h>
 #include <utils/string.h>
@@ -8,19 +7,41 @@
 int check_authorized(holyreq_t *req);
 void init_cgi(void);
 
-static char *get_tmpl_path()
+static char *dirname(char *path)
+{
+    static char dir[256] = {0};
+    int i, len = strlen(path);
+    
+    for (i = len; i >= 0; --i) {
+        if (path[i] == '/') {
+            break;
+        }
+    }
+    
+    if (i == 0) {
+        return "/";
+    }
+    
+    if (i < 0) {
+        return ".";
+    }
+
+    memcpy(dir, path, i);
+    dir[i] = 0;
+    return dir;
+}
+
+static char *get_tmpl_path(char *path)
 {
     static char buf[256] = {0};
-    getcwd(buf, sizeof buf);
-    STR_APPEND(buf, sizeof buf, "/template");
+    STR_APPEND(buf, sizeof buf, "%s/template", dirname(path));
     return buf;
 }
 
-static char *get_static_path()
+static char *get_static_path(char *path)
 {
     static char buf[256] = {0};
-    getcwd(buf, sizeof buf);
-    STR_APPEND(buf, sizeof buf, "/static");
+    STR_APPEND(buf, sizeof buf, "%s/static", dirname(path));
     return buf;
 }
 
@@ -30,8 +51,8 @@ int main(int argc, char *argv[])
         .port = 10000,
         .socket_timeout = 60,
         .session_timeout = 3600,
-        .template_path = get_tmpl_path(),
-        .static_path = get_static_path(),
+        .template_path = get_tmpl_path(argv[0]),
+        .static_path = get_static_path(argv[0]),
     };
     
     holyhttp_init(&cfg);
