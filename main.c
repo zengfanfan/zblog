602f1e071cc605591eaf89819ddeafe80566a7c1
Zeng Fanfan <hurryzeng@foxmail.com>
2017-10-07 23:52:18 +0800

    make login args configurable

--- main.c
+++ main.c
@@ -4,6 +4,9 @@
 #include <lib/holyhttp.h>
 #include <utils/string.h>
 
+extern char *username;
+extern char *password;
+
 int check_authorized(holyreq_t *req);
 void init_cgi(void);
 
@@ -54,7 +57,14 @@ int main(int argc, char *argv[])
         .template_path = get_tmpl_path(argv[0]),
         .static_path = get_static_path(argv[0]),
     };
-    
+
+    if (argc > 1) {
+        username = argv[1];
+    }
+    if (argc > 2) {
+        password = argv[2];
+    }
+
     holyhttp_init(&cfg);
     holyhttp_set_debug_level(HOLY_DBG_DETAIL);
     holyhttp_set_prerouting(check_authorized);
